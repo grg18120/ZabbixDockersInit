85e898165eb8be1c6b82f69c3ec5d53acb06a2ee
grg18120 <up1046903@ac.upatras.gr>
2025-08-22 14:14:45 +0300

    create docker mysql

--- main.sh
+++ main.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 PROJECT_PATH="$(pwd)"
 
+#Imports
 source "$PROJECT_PATH/config.sh"
-# source "$PROJECT_PATH/lib/dockers_utils.sh"
+source "$PROJECT_PATH/lib/dockers_utils.sh"
 
-# delete_container "$container_zabbix_mysql_server"
-
-
-
+# Dockers
+delete_container "$container_zabbix_mysql_server"
 sh "$PROJECT_PATH/zabbixDockersInit/init-$container_zabbix_mysql_server.sh" \
-    -r "$PROJECT_PATH" \
-	-c "$container_zabbix_mysql_server"
-
-# sh "$PROJECT_PATH/zabbixDockersInit/init-$container_zabbix_mysql_server.sh" \
-#   -r "$PROJECT_PATH" \
-# 	-c "$container_zabbix_mysql_server" \
-# 	-d "$mysql_db_name" \
-# 	-r "$my_sql_root_pass" \
-# 	-u "$mysql_user" \
-# 	-p "$mysql_user_pass" \
-# 	-v "$containers_volumes" \
-# 	-n "$containers_network"
+  -r "$PROJECT_PATH" \
+	-c "$container_zabbix_mysql_server" \
+	-d "$mysql_db_name" \
+	-r "$my_sql_root_pass" \
+	-u "$mysql_user" \
+	-p "$mysql_user_pass" \
+	-v "$containers_volumes" \
+	-n "$containers_network"
 
 # Keep the terminal open until you press Enter
 read
--- zabbixDockersInit/init-zabbix-mysql-server.sh
+++ zabbixDockersInit/init-zabbix-mysql-server.sh
@@ -15,22 +15,16 @@ do
     esac
 done
 
-
-
-source "$PROJECT_PATH/lib/dockers_utils.sh"
-delete_container "$container_zabbix_mysql_server"
-
-
 # Run Docker
-# docker run --name $container -t \
-#   -v $containers_volumes/$container/mysqlfiles:/var/lib/mysql \
-#   -e MYSQL_DATABASE="$mysql_db_name" \
-#   -e MYSQL_ROOT_PASSWORD="$my_sql_root_pass" \
-#   -e MYSQL_USER="$mysql_user" \
-#   -e MYSQL_PASSWORD="$mysql_user_pass" \
-#   --network=$containers_network \
-#   --restart unless-stopped \
-#   -d mysql:8.0-oracle \
-#   --character-set-server=utf8mb4 \
-#   --collation-server=utf8mb4_unicode_ci \
-#   --default-authentication-plugin=caching_sha2_password
+docker run --name $container -t \
+  -v $containers_volumes/$container/mysqlfiles:/var/lib/mysql \
+  -e MYSQL_DATABASE="$mysql_db_name" \
+  -e MYSQL_ROOT_PASSWORD="$my_sql_root_pass" \
+  -e MYSQL_USER="$mysql_user" \
+  -e MYSQL_PASSWORD="$mysql_user_pass" \
+  --network=$containers_network \
+  --restart unless-stopped \
+  -d mysql:8.0-oracle \
+  --character-set-server=utf8mb4 \
+  --collation-server=utf8mb4_unicode_ci \
+  --default-authentication-plugin=caching_sha2_password
